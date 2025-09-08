c16e7b1fab088157f739321275bd5afb3f3ca53a
xvvas <miguelvg097@gmail.com>
2025-09-07 23:40:11 -0500

    Add admin registration, user registration view, and blog post URLs

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from django.contrib.auth import get_user_model
+from .serializers import UserRegisterSerializer
+from rest_framework_simplejwt.views import TokenObtainPairview, TokenRefreshView
 
-# Create your views here.
+User = get_user_model()
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserRegisterSerializer
+    permission_classes = [permissions.AllowAny]
+    
