f9e5dcf98b3bf0ee19ef6f1237755986ffe5a9d8
xvvas <miguelvg097@gmail.com>
2025-09-07 21:25:03 -0500

    Fix syntax error in update_post function definition

--- blog/services.py
+++ blog/services.py
@@ -3,7 +3,7 @@ from .models import Post
 def create_post(author, title, content):
     post = Post.objects.create(author=author, title=title, content=content)
     return post
-def update_post(instance: Post, title=title, content=content)
+def update_post(instance: Post, title=title, content=content):
     instance.title = title
     instance.content = content 
     instance.save()
